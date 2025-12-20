03f7c0f8a9d53f87d868e42fc3f491eb416139f3
Parallax73 <narakagaara@gmail.com>
2025-12-19 23:36:44 -0300

    Added library binding

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
-}
+mod engine; 
+
+use pdfium_render::prelude::*;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let pdfium = engine::init_pdfium()?;
+
+    let document = pdfium.load_pdf_from_file("test/form-test.pdf", None)?;
+
+    document
+        .pages()
+        .iter()
+        .enumerate()
+        .for_each(|(index, page)| {
+            println!("=============== Page {} ==============", index);
+            
+            let text = page.text().unwrap().all(); 
+            println!("{}", text);
+        });
+
+    Ok(())
+}}
